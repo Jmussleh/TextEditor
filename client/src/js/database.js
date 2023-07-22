b28d2bb8b99204083ddb6cb895f32f39818f2190
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-07-22 08:00:37 -0400

    Added logic that gets all content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -24,6 +24,15 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from Database');
+  const Jdb = await openDB('jate', 1);
+  const tx = Jdb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('Data retrieved from the database', result.value);
+  return result?.value;
+};
 
 initdb();
